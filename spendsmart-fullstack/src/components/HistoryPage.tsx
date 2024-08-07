423d31e3d9ab265b297ede83eaffcb2b8823ab31
LucasJann <lucas.jan.jobs@gmail.com>
2024-08-06 22:10:11 -0300

    Adjusts

--- spendsmart-fullstack/src/components/HistoryPage.tsx
+++ spendsmart-fullstack/src/components/HistoryPage.tsx
@@ -35,16 +35,20 @@ const HistoryPage = () => {
 
   const [items, setItems] = useState<itemsInterface[]>([]);
   const [itemsLength, setItemsLength] = useState<number>(0);
+  const [selectedDate, setSelectedDate] = useState<dateInterface>(dateObject);
+
+  const [filterClicked, setFilterClicked] = useState<boolean | null>(null);
+  const [financesFiltered, setFinancesFiltered] = useState<boolean>(false);
+  const [filteredItemsLength, setfilteredItemsLength] = useState<number>(0);
   const [filteredItems, setFilteredItems] = useState<filteredItemsInterface[]>(
     []
   );
-  const [selectedDate, setSelectedDate] = useState<dateInterface>(dateObject);
-  const [financesFiltered, setFinancesFiltered] = useState<boolean>(false);
-  const [filteredItemsLength, setfilteredItemsLength] = useState<number>(0);
+  const [incomeBackground, setIncomeBackground] =
+    useState<string>("bg-green-600");
+  const [expenseBackground, setExpenseBackground] =
+    useState<string>("bg-red-600");
 
-  const [filterClicked, setFilterClicked] = useState<boolean | null>(null);
   const [backgroundTransition, setBackgroundTransition] = useState<string>("");
-  const [background, setBackground] = useState<boolean | null>(null);
   const [searchMessage, setSearchMessage] = useState<string>("Search By Date");
 
   useEffect(() => {
@@ -102,9 +106,7 @@ const HistoryPage = () => {
     }
   };
 
-  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
+  const submitHandler = () => {
     if (filterClicked !== null) {
       if (filterClicked) {
         const incomeItems = items.filter((e) => {
@@ -165,15 +167,17 @@ const HistoryPage = () => {
     const id = e.currentTarget.id;
     console.log(id);
     if (id === "incomeButton") {
-      setBackground(true);
+      setIncomeBackground("bg-gray-500");
+      setExpenseBackground("bg-red-600");
       setFilterClicked(true);
       setBackgroundTransition("bg-green-600");
       setSearchMessage("Search Incomes By Date");
     } else {
-      setBackground(false);
+      setExpenseBackground("bg-gray-500");
+      setIncomeBackground("bg-green-600");
       setFilterClicked(false);
       setSearchMessage("Search Expenses By Date");
-      setBackgroundTransition("bg-red-500");
+      setBackgroundTransition("bg-red-600");
     }
   };
 
@@ -190,9 +194,7 @@ const HistoryPage = () => {
             <Button
               id="incomeButton"
               type="button"
-              className={`w-1/2 p-1 m-1 rounded-md transition duration-700 ease-in-out ${
-                background ? "bg-gray-600" : "bg-green-600"
-              }`}
+              className={`w-1/2  ${incomeBackground} p-1 m-1 rounded-md transition duration-700 ease-in-out`}
               onClick={filter}
             >
               Incomes
@@ -200,9 +202,7 @@ const HistoryPage = () => {
             <Button
               id="expenseButton"
               type="button"
-              className={`w-1/2 p-1 m-1 rounded-md transition duration-700 ease-in-out ${
-                background ? "bg-red-600" : "bg-gray-600"
-              }`}
+              className={`w-1/2  ${expenseBackground} p-1 m-1 rounded-md transition duration-700 ease-in-out`}
               onClick={filter}
             >
               Expenses
