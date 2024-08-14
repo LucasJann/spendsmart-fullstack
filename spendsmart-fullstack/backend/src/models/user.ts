4c32e2d8cac227f3dfc172040bc14f3523f5da0a
LucasJann <lucas.jan.jobs@gmail.com>
2024-08-14 17:05:28 -0300

    Adjustments

--- spendsmart-fullstack/backend/src/models/user.ts
+++ spendsmart-fullstack/backend/src/models/user.ts
@@ -26,10 +26,10 @@ interface User extends Document {
 }
 
 const ItemSchema = new Schema({
-  date: { type: String, required: true },
+  date: { type: String },
   income: { type: String },
   expense: { type: String },
-  category: { type: String, required: true },
+  category: { type: String, },
 });
 
 const GoalSchema = new Schema({
